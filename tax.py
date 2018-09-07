976460776d8629ca59f7e263d5cbd6e7b8306e42
cormoran <cormoran707@gmail.com>
2018-09-07 14:46:25 +0900

    入力を処理する適当な実装を追加

--- tax.py
+++ tax.py
@@ -3,3 +3,12 @@ def calc_price(values):
     if len(values) > 0:
         assert 0 <= min(values) and max(values) <= 100 * 10000
     return int(sum(values) * 1.10 + 0.5)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            values = list(map(int, input().split(',')))
+            print(calc_price(values))
+        except EOFError:
+            break
