4f5e03586214b8100cfbef00b274556d29b898be
福田 <zionbanzai3@gmail.com>
2018-09-07 14:10:28 +0900

    リファクタリング

--- stack.py
+++ stack.py
@@ -1,13 +1,16 @@
 class Stack():
+    def __init__(self):
+        self.size_ = 0
 
     def is_empty(self):
-        return True
+        return self.size_ == 0
 
     def push(self, value):
         self.value = value
+        self.size_ += 1
 
     def top(self):
         return self.value
 
     def size(self):
-        return 1
+        return self.size_
