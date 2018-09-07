ec43af4f7a957fce6bc1c70c67c71dac75324fcc
cormoran <cormoran707@gmail.com>
2018-09-07 12:46:54 +0900

    test_push_and_size まで

--- stack.py
+++ stack.py
@@ -2,3 +2,12 @@ class Stack():
 
     def is_empty(self):
         return True
+
+    def push(self, value):
+        self.value = value
+
+    def top(self):
+        return self.value
+
+    def size(self):
+        return 1
--- test/stack.py
+++ test/stack.py
@@ -4,6 +4,18 @@ from stack import Stack
 
 class TestStack(unittest.TestCase):
 
+    def setUp(self):
+        self.stack = Stack()
+
     def test_create(self):
-        stack = Stack()
-        self.assertTrue(stack.is_empty())
+        self.assertTrue(self.stack.is_empty())
+
+    def test_push_and_top(self):
+        self.stack.push(1)
+        self.assertEqual(1, self.stack.top())
+
+    def test_push_and_size(self):
+        self.stack.push(1)
+        self.assertEqual(1, self.stack.size())
+        self.stack.push(2)
+        self.assertEqual(2, self.stack.size())
