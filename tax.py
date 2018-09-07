5ac38900c95f4e2e7f8bbb1475e5875fcee92f4f
cormoran <cormoran707@gmail.com>
2018-09-07 14:37:34 +0900

    テスト追加

--- tax.py
+++ tax.py
@@ -1,2 +1,5 @@
 def calc_price(values):
+    assert type(values) == list
+    if len(values) > 0:
+        assert 0 <= min(values) and max(values) <= 100 * 10000
     return int(sum(values) * 1.10 + 0.5)
